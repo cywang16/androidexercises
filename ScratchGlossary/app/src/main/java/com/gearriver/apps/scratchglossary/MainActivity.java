8c99fb533ee4cb9b033293717eb3e6fb804fc010
Chun-Yen Wang <chunywa@microsoft.com>
2017-12-27 14:48:47 -0800

    add 2nd sctivity

--- ScratchGlossary/app/src/main/java/com/gearriver/apps/scratchglossary/MainActivity.java
+++ ScratchGlossary/app/src/main/java/com/gearriver/apps/scratchglossary/MainActivity.java
@@ -2,12 +2,30 @@ package com.gearriver.apps.scratchglossary;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    EditText et_Keyword = null, et_Info = null;
+    TextView tv_Display = null;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et_Keyword = (EditText) findViewById(R.id.et_keyword);
+        et_Info = (EditText) findViewById(R.id.et_info);
+    }
+
+    public void findItemClicked(View view)
+    {
+    }
+
+    public void addItemClicked(View view)
+    {
     }
 }
