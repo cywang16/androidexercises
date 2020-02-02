525780a7e144967cb274a906a248c6332f33e124
Chun-Yen Wang <cywang16@gmail.com>
2020-02-02 11:46:30 -0800

    update

---
+++ BasicWebCall/app/src/main/java/com/gearriver/cywang16/basicwebcall/queryResults.java
@@ -0,0 +1,13 @@
+package com.gearriver.cywang16.basicwebcall;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class queryResults extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_query_results);
+    }
+}
