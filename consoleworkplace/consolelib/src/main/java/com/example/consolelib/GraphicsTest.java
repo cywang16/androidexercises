4783ed2c9c5b1fc69c393e9828dd98a3940b6528
Chun-Yen Wang <cywang16@gmail.com>
2020-04-06 18:05:29 -0700

    add GraphicsTest.java

---
+++ consoleworkplace/consolelib/src/main/java/com/example/consolelib/GraphicsTest.java
@@ -0,0 +1,14 @@
+package com.example.consolelib;
+
+import java.awt.*;
+import java.awt.event.*;
+
+public class GraphicsTest extends Frame {
+    public static void main(String args[]) {
+        // create frame
+        Frame myFrame = new Frame();
+        myFrame.setSize(400, 300);
+        myFrame.setTitle("Graphics Testing");
+        myFrame.setVisible(true);
+    }
+}
