e41aeb1f6ea257815627ce8f483c7a6213cfcb8f
Chun-Yen Wang <cywang16@gmail.com>
2021-04-09 21:56:55 -0700

    update

--- cotlinscratches/HelloWorld/src/main/java/com/helloworld/MyClass.java
+++ cotlinscratches/HelloWorld/src/main/java/com/helloworld/MyClass.java
@@ -3,6 +3,12 @@ package com.helloworld;
 public class MyClass {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        for (String s: args) {
+            System.out.println(s);
+        }
+        String[] sDim = args[0].split("_");
+        int dimR = Integer.parseInt(sDim[0]), dimC = Integer.parseInt(sDim[1]);
+        System.out.println(String.format("%d * %d", dimR, dimC));
     }
 }
 
